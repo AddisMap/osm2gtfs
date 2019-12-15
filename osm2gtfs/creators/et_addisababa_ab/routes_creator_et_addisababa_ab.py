28c1c23de09a98c7aa7d8f8424dc805c0d9e1acb
Alexander Menk <alex@addismap.com>
2019-12-15 11:58:29 +0100

    Trying to use different folders per agency

---
+++ osm2gtfs/creators/et_addisababa_ab/routes_creator_et_addisababa_ab.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+class RoutesCreatorEtAddisababaAb(RoutesCreatorEtAddisababa):
+    """ Stub for agency AB
+
---
+++ osm2gtfs/creators/et_addisababa_ab/schedule_creator_et_addisababa_ab.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+
+from osm2gtfs.creators.schedule_creator_et_addisababa import ScheduleCreatorEtAddisababa
+
+class ScheduleCreatorEtAddisababaAb(ScheduleCreatorEtAddisababa):
+    """ Stub for agency AB
